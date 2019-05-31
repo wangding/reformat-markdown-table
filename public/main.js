23c443ef0a0c89c01f459d719e949a49713f25da
wangding <408542507@qq.com>
2019-05-31 14:00:47 +0800

    feat: add fork-me-github UI widget

--- public/main.js
+++ public/main.js
@@ -4,9 +4,11 @@ var reformatBtn, mdTable,
     reformat = require('../lib/reformat-table.js');
 
 /*eslint-env browser*/
+/* global $forkMeGH */
 window.onload = function() {
   reformatBtn = document.getElementById('reformat-btn');
   mdTable = document.getElementById('md-table');
+  $forkMeGH.show('https://github.com/wangding/reformat-markdown-table');
 
   reformatBtn.onclick = function() {
     mdTable.value = reformat(mdTable.value);
