a4e77a0a0d2da4ad8a018cc805b49a300e3afd78
wangding <408542507@qq.com>
2019-06-01 12:06:24 +0800

    feat: add bsz-page-footer UI widget

--- public/main.js
+++ public/main.js
@@ -4,11 +4,12 @@ var reformatBtn, mdTable,
     reformat = require('../lib/reformat-table.js');
 
 /*eslint-env browser*/
-/* global $forkMeGH */
+/* global $forkMeGH, $bszPageFooter */
 window.onload = function() {
   reformatBtn = document.getElementById('reformat-btn');
   mdTable = document.getElementById('md-table');
   $forkMeGH.show('https://github.com/wangding/reformat-markdown-table');
+  $bszPageFooter.show('body');
 
   reformatBtn.onclick = function() {
     mdTable.value = reformat(mdTable.value);
